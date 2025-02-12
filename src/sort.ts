09b2feb07731e8cd73ac3506909e3428a7bc5b5e
Si0dot7 <felm27010@gmail.com>
2025-02-12 21:11:42 +0700

    update use function in sort.ts

--- src/sort.ts
+++ src/sort.ts
@@ -32,15 +32,15 @@ export function quickSort<T extends Input>(arr: T[]): T[] {
 }
 
 
-// function sortNumber<T extends Input>(arr1: T[],arr2: T[],arr3: T[]): void{
-//     console.log(quickSort(mergeArray(arr1,arr2,arr3)));
-// }
+function sortNumber<T extends Input>(arr1: T[],arr2: T[],arr3: T[]): void{
+    console.log(quickSort(mergeArray(arr1,arr2,arr3)));
+}
 
-// function sortString<T extends Input>(arr1: T[],arr2: T[],arr3: T[]): void{
-//     console.log(quickSort(mergeArray(arr1,arr2,arr3)));
+function sortString<T extends Input>(arr1: T[],arr2: T[],arr3: T[]): void{
+    console.log(quickSort(mergeArray(arr1,arr2,arr3)));
     
-// }
+}
 
-// sortNumber([1,2,3],[5,6,7],[6,4,3])
-// sortString(['table','milk','me'],['cat','dog','moodang'],['sun','moon','venut'])
+sortNumber([1,2,3],[5,6,7],[6,4,3])
+sortString(['table','milk','me'],['cat','dog','moodang'],['sun','moon','venut'])
 
